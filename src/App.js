2bc8c311d889d07a45f46fc379df97e1c9a57628
seppe <r0787714@thomasmore.be>
2020-09-22 17:19:05 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+        <h1>menu</h1>
+        <div>cola</div>
+        <div>water</div>
+        <div>bier</div>
+        <div>wijn</div>
+    </div>
+  );
+}
+
+export default App;
