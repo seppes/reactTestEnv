51e3fabad374a4b9a75bbdad773c8b669e125651
seppe <r0787714@thomasmore.be>
2020-09-23 18:44:19 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import './App.css';
 
+function ProductLine(props) {
+    const {product} = props;
+    return <div>{product}{props.prijs}</div>;
+}
+
 function App() {
   return (
     <div>
-        <h1>menu</h1>
-        <div>cola</div>
-        <div>water</div>
-        <div>bier</div>
-        <div>wijn</div>
+        <h1>Menu</h1>
+        <ProductLine product="cola" prijs={"1"}/>
+        <ProductLine product="water" prijs={"2"}/>
+        <ProductLine product="bier" prijs={"3"}/>
+        <ProductLine product="wijn" prijs={"4"}/>
     </div>
   );
 }
