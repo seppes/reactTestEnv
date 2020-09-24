870d1c4ef8720d12a92c3c889c750e52cf1c63e7
seppe <r0787714@thomasmore.be>
2020-09-24 15:03:50 +0200

    producten in array

--- src/App.js
+++ src/App.js
@@ -2,20 +2,29 @@ import React from 'react';
 import './App.css';
 
 function ProductLine(props) {
-    const {product} = props;
-    return <div>{product}{props.prijs}</div>;
-}
+    const {product, prijs} = props;
 
+    return <div className="productLine">
+        <div>{product.name}</div>
+        <div>{product.price}&euro;</div>
+    </div>;
+}
+const PRODUCTS_DATA = [
+    {name: "cola", price: 2},
+    {name: "water", price: 2},
+    {name: "bier", price: 2},
+    {name: "wijn", price: 4}
+];
 function App() {
-  return (
-    <div>
-        <h1>Menu</h1>
-        <ProductLine product="cola" prijs={"1"}/>
-        <ProductLine product="water" prijs={"2"}/>
-        <ProductLine product="bier" prijs={"3"}/>
-        <ProductLine product="wijn" prijs={"4"}/>
-    </div>
-  );
+    return (
+        <div>
+            <h1>Menu</h1>
+            <ProductLine product={PRODUCTS_DATA[0]}/>
+            <ProductLine product={PRODUCTS_DATA[1]}/>
+            <ProductLine product={PRODUCTS_DATA[2]}/>
+            <ProductLine product={PRODUCTS_DATA[3]}/>
+        </div>
+    );
 }
 
 export default App;
