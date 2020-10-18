712090deda0c3a4de035f8d874fc68eb12cb7a38
$GITHUB_ACTOR <$GITHUB_ACTOR@bots.github.com>
2020-10-18 14:41:44 +0000

    Updates

---
+++ precache-manifest.e0b27b1820031444e6f80c683a39f706.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01f16336aec913de171f4f8461cce4b6",
+    "url": "/menucard/index.html"
+  },
+  {
+    "revision": "1b9c9063a71dae13cb2b",
+    "url": "/menucard/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "bc4447ec7da98710d907",
+    "url": "/menucard/static/css/main.e50606ab.chunk.css"
+  },
+  {
+    "revision": "1b9c9063a71dae13cb2b",
+    "url": "/menucard/static/js/2.a579df5d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/menucard/static/js/2.a579df5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc4447ec7da98710d907",
+    "url": "/menucard/static/js/main.2329652f.chunk.js"
+  },
+  {
+    "revision": "39f5a1cd608593fdedcb",
+    "url": "/menucard/static/js/runtime-main.52e723cb.js"
+  }
+]);
